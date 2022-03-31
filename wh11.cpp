1c8f11dd9e1d04a756cc5e57a08e2c4ed9af5941
MaheshDhame07 <102727584+MaheshDhame07@users.noreply.github.com>
2022-03-31 14:34:03 +0530

    Add files via upload

---
+++ wh11.cpp
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+main()
+{
+	int n,i=1,sum=0;
+	printf("\n\t enter the number =");
+	scanf("%d",&n);
+	while(i<=10)
+	{
+		sum=sum+n;
+		printf("\n\t %d",sum);
+		
+		i++;
+	}
+	
+	
+}
